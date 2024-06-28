d1202d1f8231b8b042e33895c3545d2b0faa5eda
nestorcastillo07 <garaynestorcastillo7@gmail.com>
2024-06-28 15:08:16 -0600

    Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,20 @@
+public class Cliente {
+
+    public String NamedeCliente;
+    public String NumdeCliente;
+
+    public Cliente (String NamedeCliente, String NumdeCliente){
+        this.NamedeCliente = NamedeCliente;
+        this.NumdeCliente = NumdeCliente;
+
+    }
+
+    public void mostrardatosdeCliente(){
+        System.out.println("Nombre del Cliente "+ this.NamedeCliente);
+        System.out.println("Numero de Cliente " + this.NumdeCliente);   
+}
+
+    public void Mostrarcarritodecompra(){
+        System.out.println("Producto ");
+    }
+}
