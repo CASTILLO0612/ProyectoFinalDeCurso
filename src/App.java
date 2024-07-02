3be4e7d3c910ba67af4f338ebd565bdef91a93ce
angel.flores894@std.uni.edu.ni <angelfloresgranada7@gmail.com>
2024-07-02 09:13:25 -0600

    Cambios

--- src/App.java
+++ src/App.java
@@ -1,10 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
-        Producto.agregarProducto();
-        Producto.mostrandoProducts();
-        Producto.modificandoProducto();
-        Producto.eliminandoProducts();
     }
 }
