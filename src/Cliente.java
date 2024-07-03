285349bdf890b014db0e050eeb3ecb1f4ae6d79d
nestorcastillo07 <garaynestorcastillo7@gmail.com>
2024-07-03 11:52:37 -0600

    cambiando otra

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,20 +2,20 @@ public class Cliente {
 
     public String NamedeCliente;
     public String NumdeCliente;
+    public int CantidadProducts;
 
-    public Cliente (String NamedeCliente, String NumdeCliente){
+    public Cliente (String NamedeCliente, String NumdeCliente
+    , int CantidadProducts){
         this.NamedeCliente = NamedeCliente;
         this.NumdeCliente = NumdeCliente;
+        this.CantidadProducts = CantidadProducts;
 
     }
-
-    public void mostrardatosdeCliente(){
+    public void mostrarCliente(){
         System.out.println("Nombre del Cliente "+ this.NamedeCliente);
-        System.out.println("Numero de Cliente " + this.NumdeCliente);   
+        System.out.println("Numero de Cliente " + this.NumdeCliente); 
+        System.out.println("Cantidad a comprar: "+ this.CantidadProducts);
 }
 
-    public void Mostrarcarritodecompra(){
-        System.out.println("Producto ");
-    }
     
 }
